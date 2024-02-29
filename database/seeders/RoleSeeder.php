6c80dffc91a810dd6b8338af5a4b842fab67a222
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2024-02-29 15:58:13 +0700

    addSeeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->createRoleIfNotExists('admin');
+        $this->createRoleIfNotExists('staff');
+        $this->createRoleIfNotExists('user');
+    }
+
+    private function createRoleIfNotExists(string $roleName): void
+    {
+        $role = Role::where('name', $roleName)->first();
+
+        if (!$role) {
+            Role::create(['name' => $roleName]);
+            $this->command->info("Role '{$roleName}' created successfully.");
+        } else {
+            $this->command->info("Role '{$roleName}' already exists.");
+        }
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Check if the user with the specified username already exists
+        $existingUser = User::where('username', 'admin')->first();
+
+        if (!$existingUser) {
+            // User doesn't exist, so create it
+            $adminUser = User::create([
+                'name' => 'Admin',
+                'username' => 'admin',
+                'password' => bcrypt('iddrivesadmin'), // Use bcrypt() to hash the password
+                'agn' => '',
+                'brn' => '',
+                'dpm' => '',
+                'role' => 'admin',
+                'icon' => '',
+            ]);
+
+            if (!($adminUser->hasRole('admin'))) {
+                $adminUser->assignRole('admin');
+            }
+
+            $this->command->info("User Admin created successfully.");
+        } else {
+            // User already exists, display a message
+            $this->command->info("User Admin already exists.");
+        }
+    }
+}
