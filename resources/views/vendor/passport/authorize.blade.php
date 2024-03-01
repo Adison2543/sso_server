685e076b0c8e6f9e95ebba67a5bdad406673d671
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2024-03-01 11:39:38 +0700

    addTabIcon

--- resources/views/vendor/passport/authorize.blade.php
+++ resources/views/vendor/passport/authorize.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>{{ config('app.name') }} - Authorization</title>
+    <link rel="icon" type="image/x-icon" href="/imgs/logoid_new.png">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
