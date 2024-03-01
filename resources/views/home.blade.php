727a4d085750347297efeb9a122fa2a12381b067
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2024-03-01 10:25:05 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
 
                     <div class="pt-3 container text-center">
                         <div class="row row-cols-sm-2 row-cols-md-4">
-                            <a href="https://dronettc.com/admin" target="_BLANK" class="link-underline link-underline-opacity-0 link-dark">
+                            <a href="https://dronettc.com/" target="_BLANK" class="link-underline link-underline-opacity-0 link-dark">
                                 <div class="service p-2 rounded">
                                     <img src="/imgs/logoiddrives.png" width="50" alt="">
                                     <p class="mb-0">DroneTTC</p>
